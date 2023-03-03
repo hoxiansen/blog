a5a045db99d6f7f06c390b094890685464096316
hoxiansen <hoxiansen@163.com>
2023-03-03 15:39:04 +0800

    Site updated: 2023-03-03 15:39:03

--- js/layouts/toc.js
+++ js/layouts/toc.js
@@ -8,20 +8,20 @@ function initTOC() {
 
       ...REDEFINE.utils,
 
-      findActiveIndexByTOC() {
+      updateActiveTOCLink() {
         if (!Array.isArray(REDEFINE.utils.sections)) return;
         let index = REDEFINE.utils.sections.findIndex(element => {
-          return element && element.getBoundingClientRect().top - 20 > 0;
+          return element && element.getBoundingClientRect().top - 200 > 0;
         });
         if (index === -1) {
           index = REDEFINE.utils.sections.length - 1;
         } else if (index > 0) {
           index--;
         }
-        this.activateNavByIndex(index);
+        this.activateTOCLink(index);
       },
 
-      registerSidebarTOC() {
+      registerTOCScroll() {
         REDEFINE.utils.sections = [...document.querySelectorAll('.post-toc li a.nav-link')].map(element => {
           const target = document.getElementById(decodeURI(element.getAttribute('href')).replace('#', ''));
           element.addEventListener('click', event => {
@@ -43,19 +43,19 @@ function initTOC() {
         });
       },
 
-      activateNavByIndex(index) {
+
+
+      activateTOCLink(index) {
         const target = document.querySelectorAll('.post-toc li a.nav-link')[index];
-        if (!target || target.classList.contains('active-current')) return;
+
+        if ( (!target || target.classList.contains('active-current')) ) {
+          return;
+        }
 
         document.querySelectorAll('.post-toc .active').forEach(element => {
           element.classList.remove('active', 'active-current');
         });
         target.classList.add('active', 'active-current');
-        let parent = target.parentNode;
-        while (!parent.matches('.post-toc')) {
-          if (parent.matches('li')) parent.classList.add('active');
-          parent = parent.parentNode;
-        }
         // Scrolling to center active TOC element if TOC content is taller then viewport.
         const tocElement = document.querySelector('.post-toc-wrap');
         window.anime({
@@ -66,7 +66,7 @@ function initTOC() {
         });
       },
 
-      showPageAsideWhenHasTOC() {
+      showTOCAside() {
 
         const openHandle = () => {
           const styleStatus = REDEFINE.getStyleStatus();
@@ -90,11 +90,13 @@ function initTOC() {
       }
     }
 
-    REDEFINE.utils.showPageAsideWhenHasTOC();
-    REDEFINE.utils.registerSidebarTOC();
+    REDEFINE.utils.showTOCAside();
+    REDEFINE.utils.registerTOCScroll();
 
   } else {
-    REDEFINE.utils.pageContainer_dom.removeChild(document.querySelector('.toc-content-container'));
+    document.querySelectorAll('.toc-content-container, .toc-marker').forEach((elem) => {
+      elem.remove();
+    });
   }
 }
 
