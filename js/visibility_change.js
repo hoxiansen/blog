3b6f3b5ab17dfc487dacec571209e69592a1cf94
hoxiansen <hoxiansen@163.com>
2023-03-06 16:59:23 +0800

    Site updated: 2023-03-06 16:59:22

---
+++ js/visibility_change.js
@@ -0,0 +1,15 @@
+let originTitle = document.title;
+let titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        document.title = '(*σ´∀`)σ 我走噜~ ' + originTitle;
+        if (titleTime != null) {
+            clearTimeout(titleTime);
+        }
+    } else {
+        document.title = '(*´∇｀*) 我来力~ ' + originTitle;
+        titleTime = setTimeout(function () {
+            document.title = originTitle;
+        }, 2000);
+    }
+});
